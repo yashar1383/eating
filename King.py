d8e1a2cb893016ceb6ed1bfc8d878f8b89cd28b5
yashar <yashi1383@gmail.com>
2022-05-24 23:22:16 +0430

    finishd moving and eating prosses

--- King.py
+++ King.py
@@ -1,36 +1,65 @@
 from random import random
-#Queen Branch Developed main
+# end imports
+#start body
 class Gubamba():
     List = []
     population = 0
-    __stom = 0 
-    __childe = 0
-    __i : 0
-    __j : 0
+    state = False #does the Box that object is in has food
+    __belly = 0 #number of foods in Guba belly
+    __childe = 0 #number of childs Guba has
+    __i : 0 #Gubamba location(x)
+    __j : 0 #Gubamba location(y)
     def __init__(self,int:Identity) -> None:    #identity -> (selfish,1) , (unselfidh , 0)
         self.state = True   #means alive:True or Death:False
-        self.iden = Identity 
+        self.iden = Identity  #selfish:1 & unselfidh:0
         Gubamba.population += 1
         Gubamba.List.append(self)
     def get_identiy(self):
         return self.iden
     def eating(self , eat):
-        self.stom = eat
-    def move() : #about moving
-        pass
+        self.belly = eat
+    def live_Division(self): #from conditions make decision of living or creating a child
+        if self.belly == 2 :
+            self.__childe += 1
+            Gubamba.population += 1
+            Gubamba.list.append(Gubamba(self.iden))
+        if self.belly == 3/2 and random() < 0.5 : #if under 0.5 breath a child else do nithing
+            self.__childe += 1
+            Gubamba.population += 1
+            Gubamba.list.append(Gubamba(self.iden))
+        elif self.belly == 1/2 and random() < 0.5 :  #if under 0.5 die
+            del Gubamba.List[Gubamba.List.index(self)]
+            Gubamba.population -= 1
+        elif  self.belly == 1 : 
+            del Gubamba.List[Gubamba.List.index(self)]
+            Gubamba.population -= 1
+    def Housing(self): #the Box that Guba is in , has food
+        self.state = True
+    def move(self) : #about moving
+        if self.state == False:
+            if random() > 0.5:
+                if random() > 0.5:
+                    self.__i = (self.__i + 1)%m
+                else :
+                    self.__i = (self.__i +m - 1)%m
+            else :
+                if random() > 0.5:
+                    self.__j = (self.__j + 1)%n
+                else :
+                    self.__j = (self.__j +n - 1)%n
 class Box():
-    price_vale = {0:(1,1) , 1:(1/2,3/2) , 2:(1/2 , 1/2)}
+    price_vale = {0:(1,1) , 1:(1 /2,3/2) , 2:(1/2 , 1/2)}
     population = 0
     Idens = 0
     def __init__(self,i,j) -> None:
-        self.i = i
-        self.j = j
-        self.Gubamba = []
-        self.fruits = 0
-    def add_Gubamba(self , Guba): #Guba is the object 
+        self.i = i #direction i
+        self.j = j #diraction j (of Box)
+        self.Gubambas = [] #list of human in it
+        self.fruits = 0 #number of fruit in 
+    def add_Gubamba(self , Guba : Gubamba): #add guba in Box if conditions return True (Guba is the object )
         if self.fruits == 2 and self.population < 2 :
             self.population += 1
-            self.Gubamba.append(Guba)
+            self.Gubambas.append(Guba)
             self.Idens += Guba.iden
             return True
         else :
@@ -38,8 +67,8 @@ class Box():
     def add_fruit(self) -> None :
         self.add_fruit = 2
     def feed(self) -> None :
-        for Guba in self.Gubamba :
-            Guba.eating((self.population -1)*(self.price_vale[self.Idens][Guba.iden]))
+        for Guba in self.Gubambas :
+            Guba.eating(self.price_vale[self.population -1][Guba.iden])
     @staticmethod
     def initialization(n,m , t):    
         Map = []
@@ -51,7 +80,7 @@ class Box():
                     Map[-1][-1].add_fruit()
                     t -= 1
         return Map
-Gubamba(0) ; Box(2,3)
+n , m = int(input()) , int(input())
 for _ in range(10):     #each day
     for _ in range(24): #each hour
         pass
